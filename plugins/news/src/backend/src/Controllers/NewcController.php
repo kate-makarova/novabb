febd3738678cfa5dec741b8918fc852e3708465d
Kate Makarova <kate.makarova@hotmail.com>
2025-09-27 22:51:09 -0400

    Started on plugin backend

---
+++ plugins/news/src/backend/src/Controllers/NewcController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Plugins\NovaBasicNews\Controllers;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class NewcController extends AbstractController
+{
+
+}
---
+++ plugins/news/src/backend/src/Entities/News.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Plugins\NovaBasicNews\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+class News
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $name;
+
+    #[ORM\Column(type: 'text')]
+    private string $content;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): void
+    {
+        $this->content = $content;
+    }
+}
